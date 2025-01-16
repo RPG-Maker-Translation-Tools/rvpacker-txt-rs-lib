829dbbe8ec376c23e070822f28fe0adc1382a552
savannstm <savannstm@gmail.com>
2025-01-17 00:51:32 +0500

    Remove number matching from `STRING_IS_ONLY_SYMBOLS_RE` and `INVALID_VARIABLE_RE` regexps

--- src/statics.rs
+++ src/statics.rs
@@ -20,15 +20,15 @@ pub mod regexes {
     use regex::Regex;
 
     pub static STRING_IS_ONLY_SYMBOLS_RE: Lazy<Regex> = Lazy::new(|| unsafe {
-        Regex::new(r#"^[,.()+\-:;\[\]^~%&!№$@`*\/→×？?ｘ％▼|♥♪！：〜『』「」〽。…‥＝゠、，【】［］｛｝（）〔〕｟｠〘〙〈〉《》・\\#<>=_ー※▶ⅠⅰⅡⅱⅢⅲⅣⅳⅤⅴⅥⅵⅦⅶⅧⅷⅨⅸⅩⅹⅪⅺⅫⅻⅬⅼⅭⅽⅮⅾⅯⅿ\s0-9]+$"#).unwrap_unchecked()
+        Regex::new(r#"^[,.()+\-:;\[\]^~%&!№$@`*\/→×？?ｘ％▼|♥♪！：〜『』「」〽。…‥＝゠、，【】［］｛｝（）〔〕｟｠〘〙〈〉《》・\\#<>=_ー※▶ⅠⅰⅡⅱⅢⅲⅣⅳⅤⅴⅥⅵⅦⅶⅧⅷⅨⅸⅩⅹⅪⅺⅫⅻⅬⅼⅭⅽⅮⅾⅯⅿ\s]+$"#).unwrap_unchecked()
     });
     pub static ENDS_WITH_IF_RE: Lazy<Regex> = Lazy::new(|| unsafe { Regex::new(r" if\(.*\)$").unwrap_unchecked() });
     pub static LISA_PREFIX_RE: Lazy<Regex> =
-        Lazy::new(|| unsafe { Regex::new(r"^(\\et\[[0-9]+\]|\\nbt)").unwrap_unchecked() });
+        Lazy::new(|| unsafe { Regex::new(r"^(\\et\[\d+\]|\\nbt)").unwrap_unchecked() });
     pub static INVALID_MULTILINE_VARIABLE_RE: Lazy<Regex> =
-        Lazy::new(|| unsafe { Regex::new(r"^#? ?<.*>.?$|^[a-z][0-9]$").unwrap_unchecked() });
+        Lazy::new(|| unsafe { Regex::new(r"^#? ?<.*>.?$|^[a-z]\d$").unwrap_unchecked() });
     pub static INVALID_VARIABLE_RE: Lazy<Regex> =
-        Lazy::new(|| unsafe { Regex::new(r"^[+-]?[0-9]+$|^///|---|restrict eval").unwrap_unchecked() });
+        Lazy::new(|| unsafe { Regex::new(r"^[+-]?$|^///|---|restrict eval").unwrap_unchecked() });
 }
 
 /// 401 - Dialogue line.
