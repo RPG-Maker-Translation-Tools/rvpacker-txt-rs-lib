3f04c66723cfaa2334e85efac437bb48ffb5d06b
savannstm <savannstm@gmail.com>
2025-03-06 23:49:33 +0500

    Add `parse_ignore` to imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //!Library providing functions for rvpacker-txt-rs. Not intended for use in other projects, but can be.
 
 mod functions;
-pub use functions::{determine_extension, read_to_string_without_bom};
+pub use functions::{determine_extension, parse_ignore, read_to_string_without_bom};
 
 pub mod json;
 pub mod purge;
